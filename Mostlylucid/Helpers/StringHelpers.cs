490096ca99d09f8b0ca193cd22d2cbfe4ff302e8
Scott Galloway <scott@jane.com>
2024-08-07 17:13:53 +0100

    Improved guid generation based on slug (which must be unique)

--- Mostlylucid/Helpers/StringHelpers.cs
+++ Mostlylucid/Helpers/StringHelpers.cs
@@ -1,4 +1,4 @@
-﻿using System.Security.Cryptography;
+﻿using System.IO.Hashing;
 using System.Text;
 
 namespace Mostlylucid.Helpers;
@@ -14,12 +14,17 @@ public static class StringHelpers
         return endIndex > 0 ? value.Substring(0, endIndex) : value.Substring(0, length);
     }
     
-    public static Guid ToGuid(this string input)
+     public  static string ToGuid(this string  name)
     {
-        using var provider = MD5.Create();
-        var inputBytes = Encoding.UTF8.GetBytes(input);
-        var hashBytes = provider.ComputeHash(inputBytes);
-        return new Guid(hashBytes);
+        var buf = Encoding.UTF8.GetBytes(name);
+            
+                var guid = XxHash128.Hash(buf);
+
+        // Don't use Guid constructor, it tends to swap bytes. We want to preserve original string as hex dump.
+        var guidS =  string.Format("{0:X2}{1:X2}{2:X2}{3:X2}-{4:X2}{5:X2}-{6:X2}{7:X2}-{8:X2}{9:X2}-{10:X2}{11:X2}{12:X2}{13:X2}{14:X2}{15:X2}", 
+            guid[0], guid[1], guid[2], guid[3], guid[4], guid[5], guid[6], guid[7], guid[8], guid[9], guid[10], guid[11], guid[12], guid[13], guid[14], guid[15]);
+
+        return guidS.ToLowerInvariant();
     }
 
 }
--- Mostlylucid/RSS/Models/RSSFeedItem.cs
+++ Mostlylucid/RSS/Models/RSSFeedItem.cs
@@ -5,10 +5,12 @@ namespace Mostlylucid.RSS.Models;
 public class RssFeedItem
 {
     public string Title { get; set; }
+    
+    public string Slug { get; set; }
     public string Link { get; set; }
     public string Description { get; set; }
     public DateTime PubDate { get; set; }
     public string[] Categories { get; set; } // New property
 
-    public Guid Guid => Title.ToGuid();
+    public string Guid => Slug.ToGuid();
 }
