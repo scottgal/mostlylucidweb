9938c488f667bba1a97635beb00c34598a0e3153
Scott Galloway <scott@jane.com>
2024-09-13 03:23:06 +0100

    Build fixes.

--- Mostlylucid/webpack.config.js
+++ Mostlylucid/webpack.config.js
@@ -9,17 +9,13 @@ module.exports = (env, argv) => {
         mode: isProduction ? 'production' : 'development', // Set mode based on environment
         entry: './src/js/main.js', // Your entry file
         output: {
-                filename: '[name].[contenthash].js',  // Use unique name for each chunk
-                clean: true,  // To clean up old files before each build
-            path: path.resolve(__dirname, 'wwwroot/js/dist') // Corrected output directory path
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'wwwroot/js/dist'), // Corrected output directory path
         },
         resolve: {
             extensions: ['.js'], // Optional but fine to include
         },
         optimization: {
-            splitChunks: {
-                chunks: 'all',  // This splits all types of chunks
-            },
             minimize: isProduction, // Only minimize in production mode
             minimizer: isProduction ? [
                 new TerserPlugin({
@@ -44,5 +40,5 @@ module.exports = (env, argv) => {
         },
         // Add the devtool property for source maps
         devtool: isProduction ? 'source-map' : 'eval-source-map', // Use full source maps for production and eval-source-map for development
-    }
-}
+    };
+};
